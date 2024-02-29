ff18aa59c1f457435a42ad50dd2555d3e1ebf354
Ayush Sharma <Ayushsharma014may@gmail.com>
2024-02-29 23:31:23 +0530

    style: added Todo style

--- src/components/profileComponents/Todo.jsx
+++ src/components/profileComponents/Todo.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { CiMenuKebab } from "react-icons/ci";
 
 const Todo = () => {
     return (
-        <div className='todo' >
-            <div className="todo-body">
-                adsasd
+        <div className='todo rounded-4 bg-light shadow-sm text-black p-3 py-4' >
+            <div className="todo-body h-100 w-100 ">
+                <CiMenuKebab size={'1.4em'} className='menu-logo' />
+                <h3>Sample Todo</h3>
+                <table>
+                    <tbody className='mt-2'>
+                        <tr>
+                            <td> <input type="checkbox" name="" id="" /> </td>
+                            <td> sample 1 </td>
+                        </tr>
+                        <tr>
+                            <td> <input type="checkbox" name="" id="" /> </td>
+                            <td> sample 1 </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     )
