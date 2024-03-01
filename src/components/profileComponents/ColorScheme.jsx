cdc06c7478b452dc560d51cefa7a39ef5c7770c6
Ayush Sharma <Ayushsharma014may@gmail.com>
2024-03-01 14:20:20 +0530

    feat: added new compo

---
+++ src/components/profileComponents/ColorScheme.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const ColorScheme = () => {
+    const color = useSelector(state => state.color.colors);
+    console.log(color)
+    return (
+        <div className='todo-color-scheme gap-2 d-flex pt-2'>
+            {
+                color.map(element => (
+                    <div className='color-circle' style={{
+                        backgroundColor: `${element}`
+                    }} ></div>
+                ))
+            }
+        </div>
+    )
+}
+
+export default ColorScheme
---
+++ src/components/profileComponents/Type.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Type = ({type}) => {
+    return (
+        <h6 className='position-absolute' style={{ opacity: "0.5" }} >{type}</h6>
+    )
+}
+
+export default Type
