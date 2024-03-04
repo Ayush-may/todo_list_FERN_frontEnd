5102c8efd68fb2a4656724ad1da8b09380164f2b
Ayush Sharma <Ayushsharma014may@gmail.com>
2024-03-04 23:57:50 +0530

    feat: added redix-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import todoReducer from '../features/todos/todoSlice';
 import colorReducer from '../features/colors/colorSlice';
+import storage from 'redux-persist/lib/storage'
+import { persistStore } from 'redux-persist';
+import persistReducer from 'redux-persist/es/persistReducer';
 
-export const store = configureStore({
-    reducer: {
-        color: colorReducer,
-        todo: todoReducer
-    }
+const rootReducer = combineReducers({
+    color: colorReducer,
+    todo: todoReducer
 });
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistReducers = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistReducers
+});
+
+const persistor = persistStore(store);
+
+export { persistor, store };
