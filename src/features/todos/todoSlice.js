57f4569f6bd12294b261e77ca665ccad35b7c984
Ayush Sharma <Ayushsharma014may@gmail.com>
2024-03-04 16:07:51 +0530

    feat: added API call and added reducers

--- src/features/todos/todoSlice.js
+++ src/features/todos/todoSlice.js
@@ -1,16 +1,43 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
+    currentUserUID: '',
     todos: [],
     searchQueryText: ''
 }
+
+const addTodoToFirebaseDasebaseOfCurrentUser = async (todo) => {
+    const UID = localStorage.getItem('uId');
+    const data = await axios({
+        url: '/add_todo',
+        method: 'POST',
+        data: { todo, UID }
+    });
+}
+
+export const fetchTodoFromCurrentUser = createAsyncThunk('todo/fetchTodoFromCurrentUser', async () => {
+    const UID = localStorage.getItem('uId');
+    const response = await axios({
+        url: `/user/todos/${UID}`,
+        method: "GET",
+        data: { UID }
+    })
+
+    return response.data;
+})
 
 export const todoSlice = createSlice({
     name: 'todo',
     initialState: initialState,
     reducers: {
+        setCurrentUserUID: (state, action) => {
+            const UID = action.payload.UID;
+            state.currentUserUID = UID + '';
+        },
         addTodo: (state, action) => {
             const todo = { id: nanoid(), text: action.payload, time: new Date().toLocaleTimeString(), date: new Date().toLocaleDateString() };
+            addTodoToFirebaseDasebaseOfCurrentUser(todo, state.currentUserUID);
             state.todos.unshift(todo);
         },
         removeTodo: (state, action) => {
@@ -18,13 +45,33 @@ export const todoSlice = createSlice({
         },
         searchQuery: (state, action) => {
             state.searchQueryText = action.payload;
+        },
+        addTodoFromAPI: (state, action) => {
+            action.payload.map(todo => state.todos.unshift(todo))
+        },
+        resetTodoState: (state, action) => {
+            state.currentUserUID = '';
+            state.todos = [];
+            state.searchQueryText = '';
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchTodoFromCurrentUser.pending, (state, action) => {
+            console.log('pending');
+        })
+        builder.addCase(fetchTodoFromCurrentUser.fulfilled, (state, action) => {
+            
+        })
+        builder.addCase(fetchTodoFromCurrentUser.rejected, (state, action) => {
+            console.log('rejected');
+        })
     }
 });
+
 
 export const selectedSearchQuery = state => {
     return state.todo.todos.filter(todo => todo.text.toLowerCase().includes(state.todo.searchQueryText.toLowerCase()))
 }
 
-export const { addTodo, removeTodo, searchQuery } = todoSlice.actions;
+export const { addTodo, removeTodo, searchQuery, setCurrentUserUID, resetTodoState } = todoSlice.actions;
 export default todoSlice.reducer;
